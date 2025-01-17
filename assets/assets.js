65bc3554f425a61624354fb1053fd7144511c5b8
Irfan Ahmad <irfan.ahmad.22@gmail.com>
2025-01-18 04:34:16 +0530

    Day1 1.18 mins

--- assets/assets.js
+++ assets/assets.js
@@ -107,8 +107,8 @@ export const serviceData = [
 ]
 
 export const infoList = [
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript React Js, Next Js' },
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Computer Science' },
+    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'JavaScript, Cypress, Java, Selenium WebDriver, Cucumber, React Js' },
+    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Com from CSJM University' },
     { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
 ];
 
