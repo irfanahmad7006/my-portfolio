a414d51f1e817dfca2f65be0bcf2fd10060f0ddd
Irfan Ahmad <irfan.ahmad.22@gmail.com>
2025-01-18 20:06:11 +0530

    Day2 1.37.21

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,3 +1,5 @@
+
+import { workData } from '@/assets/assets'
 import React from 'react'
 
 const Work = () => {
@@ -5,10 +7,18 @@ const Work = () => {
         <div id='work' className='w-full px-[12%] py-10 scroll-mt-20'>
             <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
             <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
-            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>4+ years of experience in Quality 
-                Assurance with hands-on Experience in Agile methodology, Functional, Database Testing and Test 
-                Management. Along with managing multiple projects individually & as a team.
+            <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>Welcome to my Test/Web Development 
+                portfolio! Explore a collection of projects showcasing my expertise in automation test/front end develpment.
             </p>
+            <div>
+                {
+                    workData.map(({title,description,bgImage},index)=>(
+                        <div>
+                            {title}  {description}   {bgImage}
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
