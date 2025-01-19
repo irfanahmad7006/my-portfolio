e210477115de54beec890c88d25e887ca1d533d3
Irfan Ahmad <irfan.ahmad.22@gmail.com>
2025-01-19 23:47:59 +0530

    refactored the download file

--- components/Header.jsx
+++ components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
                     <Image src={assets.right_arrow_white} alt='' className='w-4'/>
                 </a>
 
-                <a href="/CV_UPDATED_PIC.pdf" download className='px-10 py-3 border rounded-full
+                <a href="CV_UPDATED_PIC.pdf" download className='px-10 py-3 border rounded-full
                  border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
                 My Resume
                     <Image src={assets.download_icon} alt='' className='w-4'/>
