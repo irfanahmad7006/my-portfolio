d6ba35b3f0af257fde5b6f4f8f46a6c685f6c898
Irfan Ahmad <irfan.ahmad.22@gmail.com>
2025-01-20 00:53:45 +0530

    day3. 3.21 added motions.

--- components/About.jsx
+++ components/About.jsx
@@ -1,54 +1,79 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets, infoList, toolsData } from '@/assets/assets'
+import { motion } from "motion/react"
 
-const About = ({isDarkMode}) => {
+const About = ({ isDarkMode }) => {
     return (
-        <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
-            <h4 className='text-center mb-2 text-lg font-Ovo'>Introduction</h4>
-            <h2 className='text-center text-5xl font-Ovo'>About me</h2>
-            <div className='flex w-full flex-col lg:flex-row items-center 
-            gap-20 my-20'>
-                
-                <div className='w-64 sm:w-80 rounded-3xl max-x-none'>
+        <motion.div id='about' className='w-full px-[12%] py-10 scroll-mt-20'
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+        >
+            <motion.h4 
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+            className='text-center mb-2 text-lg font-Ovo'>Introduction</motion.h4>
+
+            <motion.h2 
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className='text-center text-5xl font-Ovo'>About me</motion.h2>
+
+            <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
+
+                <motion.div 
+                initial={{ opacity: 0, scale: 0.9 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.6 }}
+                className='w-64 sm:w-80 rounded-3xl max-x-none'>
                     <Image src={assets.user_image} alt='user' className='w-full rounded-3xl' />
-                </div>
-                <div className='flex-1'>
-                    <p className='mb-10 max-w-2xl font-Ovo'> 
+                </motion.div>
+
+                <motion.div 
+                
+                className='flex-1'>
+                    <p className='mb-10 max-w-2xl font-Ovo'>
                         A dynamic professional having experience in developing automation testing scripts beginning with gathering requirements,
                         design, implementation and test phases. Presently associated as a Senior Quality Engineer with Optum. Adept in end-to-end
                         development of automation test scripts from requirement analysis to system study, designing, coding testing, de-bugging,
                         documentation and implementation
                     </p>
                     <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6
-                    max-w-2xl'>                      
+                    max-w-2xl'>
                         {
-                            infoList.map(({icon,iconDark,title,description},index)=>(
+                            infoList.map(({ icon, iconDark, title, description }, index) => (
                                 <li className='border-[0.5px] border-gray-400 rounded-xl 
                                 p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 
                                 duration-500 hover:shadow-black dark:border-white 
-                                dark:hover:shadow-white dark:hover:bg-darkHover/50' 
-                                key={index} >
-                                    <Image src={isDarkMode? iconDark :icon} alt={title} className='w-7 mt-3'/>
+                                dark:hover:shadow-white dark:hover:bg-darkHover/50'
+                                    key={index} >
+                                    <Image src={isDarkMode ? iconDark : icon} alt={title} className='w-7 mt-3' />
                                     <h3 className='my-4 font-semibold text-gray-700 dark:text-white'>{title}</h3>
                                     <p className='text-gray-600 text-sm dark:text-white/80'>{description}</p>
                                 </li>
                             ))
-                        }                        
+                        }
                     </ul>
                     <h4 className='my-6 text-gray-700 font-Ovo dark:text-white/80'>Tools I use</h4>
                     <ul className='flex items-center gap-3 sm:gap-5'>
-                        {toolsData.map((tool,index)=>(
+                        {toolsData.map((tool, index) => (
                             <li className='flex items-center justify-center w-12 sm:w-14 aspect-square border border-gray-400 
-                            rounded-lg cursor-pointer hover:-translate-y-1 duration-500' 
-                            key={index}>
-                                <Image src={tool} alt='Tools' className='w-5 sm:w-7'/>
+                            rounded-lg cursor-pointer hover:-translate-y-1 duration-500'
+                                key={index}>
+                                <Image src={tool} alt='Tools' className='w-5 sm:w-7' />
                             </li>
                         ))}
                     </ul>
-                </div>
-            </div>
-        </div>
+                </motion.div>
+            </motion.div>
+        </motion.div>
     )
 }
 
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,40 +1,68 @@
 import React from 'react'
 import Image from 'next/image'
 import { assets } from '@/assets/assets'
+import { motion } from "motion/react"
 
 const Header = () => {
 
     return (
         <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col 
         items-center justify-center gap-4'>
-            <div>
-                <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
-            </div>
-            <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
-                Hi! I am <strong className='font-Ovo'>Irfan Ahmad</strong> 
+            <motion.div
+                initial={{ scale: 0 }}
+                whileInView={{ scale: 1 }}
+                transition={{ duration: 0.8, type: 'spring', stiffness: 100 }}>
+                <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
+            </motion.div>
+
+            <motion.h3
+                initial={{ y: -20, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.3 }}
+                className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
+                Hi! I am <strong className='font-Ovo'>Irfan Ahmad</strong>
                 <Image src={assets.hand_icon} alt='' className='w-6' />
-            </h3>
-            <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
+            </motion.h3>
+
+            <motion.h1
+                initial={{ y: -30, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.5 }}
+                className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
                 Senior QA Engineer based in Delhi-NCR.
-            </h1>
-            <p className='max-w-2xl mx-auto font-Ovo'>
+            </motion.h1>
+
+            <motion.p
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 0.7 }}
+                className='max-w-2xl mx-auto font-Ovo'>
                 A dynamic professional having experience in developing automation testing scripts beginning with gathering requirements,
                 design, implementation and test phases. Presently associated as a Senior Quality Engineer with Optum. Adept in end-to-end
                 development of automation test scripts from requirement analysis to system study, designing, coding testing, de-bugging,
                 documentation and implementation
-            </p>
+            </motion.p>
+
             <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-                <a href="#contact" className='px-10 py-3 border rounded-full
+                <motion.a
+                    initial={{ y: 30, opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.6, delay: 1 }}
+                    href="#contact" className='px-10 py-3 border rounded-full
                  bg-black text-white flex items-center gap-2 dark:bg-transparent'>
                     Contact me
-                    <Image src={assets.right_arrow_white} alt='' className='w-4'/>
-                </a>
+                    <Image src={assets.right_arrow_white} alt='' className='w-4' />
+                </motion.a>
 
-                <a href="CV_UPDATED_PIC.pdf" download className='px-10 py-3 border rounded-full
+                <motion.a 
+                initial={{ y: 30, opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{ duration: 0.6, delay: 1.2 }}
+                href="CV_UPDATED_PIC.pdf" download className='px-10 py-3 border rounded-full
                  border-gray-500 flex items-center gap-2 bg-white dark:text-black'>
-                My Resume
-                    <Image src={assets.download_icon} alt='' className='w-4'/>
-                </a>
+                    My Resume
+                    <Image src={assets.download_icon} alt='' className='w-4' />
+                </motion.a>
             </div>
         </div>
     )
